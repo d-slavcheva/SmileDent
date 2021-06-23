b81b670367a4d110e938a2e99e01085c007fd538
Donika Slavcheva <slavcheva.donika@gmail.com>
2021-06-23 14:14:15 +0200

    js slide the responsive navigation

--- js/main.js
+++ js/main.js
@@ -95,19 +95,20 @@ dotsNav.addEventListener('click', e =>{
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('#mainnav .main-menu');
-    const navLinks = document.querySelectorAll('.main-menu li');
+    const navLinks = document.querySelectorAll('.main-menu > li');
 
 
     burger.addEventListener('click',() => {
 
     	nav.classList.toggle('nav-active');
-       
+        //Animate links
         navLinks.forEach((link, index) =>{
 
+            //link.style.animation = 'navLinkFade 2s ease'; link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
             if (link.style.animation) {
                 link.style.animation = '';
             } else{
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+                link.style.animation = 'navLinkFade 2s ease';
             }
         });
 
