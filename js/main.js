9d2cf7474f26c4ecc01ba9709a0bc5fef1c20d71
Donika Slavcheva <slavcheva.donika@gmail.com>
2021-06-23 10:02:21 +0200

    Animater burger icon, js not working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Caroucel
+
 const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel-button-right');
@@ -87,3 +89,31 @@ dotsNav.addEventListener('click', e =>{
    
    
 });
+
+
+// Responsible navbar
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('#mainnav .main-menu');
+    const navLinks = document.querySelectorAll('.main-menu li');
+
+
+    burger.addEventListener('click',() => {
+
+    	nav.classList.toggle('nav-active');
+       
+        navLinks.forEach((link, index) =>{
+
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else{
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+            }
+        });
+
+        burger.classList.toggle('toggle');
+
+
+    });
+}
+navSlide();
